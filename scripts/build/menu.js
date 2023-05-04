8de59c4c9325620771b478209a25e1256ef2017c
Franck Hu <byfranckhu@gmail.com>
2023-05-05 01:46:12 +0200

    style(input bar): first design for input bar

--- scripts/build/menu.js
+++ scripts/build/menu.js
@@ -2,13 +2,31 @@ export const buildMenu = () => {
   const menu = document.createElement("div");
   menu.classList.add("taby-menu");
 
-  menu.append(buildSearchInput());
+  menu.append(buildSearch());
   return menu;
+};
+
+const buildSearch = () => {
+  const search = document.createElement("div");
+  search.classList.add("taby-search");
+
+  search.appendChild(buildSearchInput());
+  return search;
 };
 
 const buildSearchInput = () => {
   const searchInput = document.createElement("input");
   searchInput.classList.add("taby-searchInput");
+  searchInput.placeholder = "Search";
 
+  searchInput.appendChild(buildSearchButton());
   return searchInput;
+};
+
+const buildSearchButton = () => {
+  const searchButton = document.createElement("button");
+  searchButton.classList.add("taby-searchButton");
+  searchButton.type = "submit";
+
+  return searchButton;
 };
