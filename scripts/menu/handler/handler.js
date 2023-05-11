1feaede247a82e18086f3395885c691ca99f1d57
Franck Hu <byfranckhu@gmail.com>
2023-05-11 19:10:33 +0200

    feat(search input): set up fuzy-search

--- scripts/menu/handler/handler.js
+++ scripts/menu/handler/handler.js
@@ -1,14 +1,45 @@
+import Fuse from "fuse.js";
+
 export const handleMenu = function (urls) {
   if (this.isDisplayed()) {
     this.displays(false);
     return;
   }
 
+  this.setUrls(urls);
   this.displays(true);
   this.focusSearchInput();
+
+  this.handleSearchItems();
+};
+
+export const handleSearchBar = function (searchInput) {
+  const options = {
+    // isCaseSensitive: false,
+    // includeScore: false,
+    // shouldSort: true,
+    // includeMatches: false,
+    // findAllMatches: false,
+    // minMatchCharLength: 1,
+    // location: 0,
+    // threshold: 0.6,
+    // distance: 100,
+    // useExtendedSearch: false,
+    // ignoreLocation: false,
+    // ignoreFieldNorm: false,
+    // fieldNormWeight: 1,
+    keys: ["title", "url"],
+  };
+
+  const fuse = new Fuse(this.urls, options);
+
+  const res = fuse.search(searchInput);
+};
+
+export const handleSearchItems = function (items) {
   this.resetSearchList();
 
-  urls.forEach((url, idx) => {
+  this.urls.forEach((url, idx) => {
     this.addSearchList(idx + ". " + url.title);
   });
 };
--- scripts/menu/menu.js
+++ scripts/menu/menu.js
@@ -2,13 +2,18 @@ import { buildMenu } from "./dom/build";
 import { contains, displays, focusSearchInput, isDisplayed } from "./dom/misc";
 import { resetSearchList } from "./dom/delete";
 import { addSearchList } from "./dom/add";
-import { handleMenu } from "./handler/handler";
+import {
+  handleMenu,
+  handleSearchBar,
+  handleSearchItems,
+} from "./handler/handler";
 
 export class Menu {
   constructor() {
+    this.urls = [];
     this.dom = buildMenu();
     this.setEvent();
-  };
+  }
 
   focusSearchInput = focusSearchInput;
   isDisplayed = isDisplayed;
@@ -17,9 +22,16 @@ export class Menu {
   addSearchList = addSearchList;
   handleMenu = handleMenu;
   contains = contains;
+  handleSearchBar = handleSearchBar;
+  handleSearchItems = handleSearchItems;
 }
 
-Menu.prototype.setEvent = function() {
-  this.dom.searchInput.addEventListener("input", function () {
+Menu.prototype.setEvent = function () {
+  this.dom.searchInput.addEventListener("input", () => {
+    this.handleSearchBar("h");
   });
-}
+};
+
+Menu.prototype.setUrls = function (urls) {
+  this.urls = urls;
+};
