4b7097efb65c8189f87d4e935377220a23d55e4a
Franck Hu <byfranckhu@gmail.com>
2023-06-28 00:53:08 +0200

    build(config): update setup ts, eslint, vite, gitignore

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import { crx } from "@crxjs/vite-plugin";
 import manifest from "./manifest.json";
 
 export default defineConfig({
-  plugins: [crx({ manifest })],
+  plugins: [react(), crx({ manifest })],
 });
