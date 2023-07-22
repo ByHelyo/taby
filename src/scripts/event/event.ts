7e4aaa71591b325d50b1bb8db3e0c30434d83477
Franck Hu <byfranckhu@gmail.com>
2023-07-22 21:27:01 +0200

    feat(menu): close menu when user switches using shortcuts or mouse

--- src/scripts/event/event.ts
+++ src/scripts/event/event.ts
@@ -5,14 +5,16 @@ import {
 } from "../../types/misc";
 import { Menu } from "../resources/menu.ts";
 
-export const eventToggleMenu = function (menu: Menu) {
+export const eventBackground = function (menu: Menu) {
   chrome.runtime.onMessage.addListener(
-    async (request: MessageFromBackground, sender) => {
-      if (
-        !sender.tab &&
-        request.type === MessageFromBackgroundType.TOGGLE_MENU
-      ) {
-        menu.isDisplayed() ? menu.closeMenu() : menu.openMenu(request.tabs);
+    async (request: MessageFromBackground) => {
+      if (request.type === MessageFromBackgroundType.TOGGLE_MENU) {
+        menu.isDisplayed()
+          ? menu.closeMenu()
+          : menu.openMenu(request.tabs || []);
+      }
+      if (request.type == MessageFromBackgroundType.USER_SWITCHES_TAB) {
+        menu.isDisplayed() && menu.closeMenu();
       }
     }
   );
