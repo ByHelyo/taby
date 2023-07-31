c3ae654e2b66b40d731cc590d77d5a95c37fdf64
Franck Hu <byfranckhu@gmail.com>
2023-07-31 14:55:31 +0200

    build(pkgs): remove react

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import { crx } from "@crxjs/vite-plugin";
 import manifest from "./manifest.json";
 
 export default defineConfig({
-  plugins: [react(), crx({ manifest })],
+  plugins: [crx({ manifest })],
 });
