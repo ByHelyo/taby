8474dc1db5895f7e1102345f60d9dfa0358eb8ed
Franck Hu <byfranckhu@gmail.com>
2023-07-31 15:01:36 +0200

    build(eslint): remove react

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,12 +3,7 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
-  plugins: ["react-refresh"],
-  rules: {
-    "react-refresh/only-export-components": "warn",
-  },
 };
