daf723b1ccf39487c4b8f9b6dc6b7cf42b5d61de
Franck Hu <byfranckhu@outlook.com>
2023-11-16 02:16:25 +0100

    refacto: remove class searchable tab

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,15 +1,16 @@
 import browser from "webextension-polyfill";
-import {
-  handleRequestSearchTab,
-  handleRequestSwitchTab,
-  handleToggleMenu,
-} from "./handler/handler";
+
 import {
   MessageFromBackground,
   MessageFromBackgroundType,
   MessageFromScript,
   MessageFromScriptType,
 } from "../type/misc.ts";
+import {
+  handleRequestSearchTab,
+  handleRequestSwitchTab,
+  handleToggleMenu,
+} from "./handler.ts";
 
 /**
  * Listens for shortcuts.
