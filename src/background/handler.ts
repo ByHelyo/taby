7e29c26d3bbf00025abd7130879170f78b23d951
Franck Hu <byfranckhu@outlook.com>
2024-02-02 00:47:44 +0100

    refacto: shortcuts

--- src/background/handler.ts
+++ src/background/handler.ts
@@ -61,14 +61,13 @@ export const handleToggleMenu = async function () {
       currentWindow: true,
     })
     .then((browserTabs) => {
-      const tabs: Tab[] = browserTabs.map((tab) => {
+      return browserTabs.map((tab) => {
         if (tab.active) {
           activeTabId = tab.id || 0;
         }
 
         return Tab.from(tab);
       });
-      return tabs;
     });
 
   const message: MessageFromBackground = {
--- src/content-scripts/component/CommandPalette.tsx
+++ src/content-scripts/component/CommandPalette.tsx
@@ -1,10 +1,54 @@
 import { Combobox, Dialog } from "@headlessui/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import {
+  MessageFromBackground,
+  MessageFromBackgroundType,
+  MessageFromScript,
+  MessageFromScriptType,
+  Tab,
+} from "../../type/misc.ts";
+import browser from "webextension-polyfill";
 
 function CommandPalette() {
-  const urls = ["asd", "asd", "asd"];
-  const [isOpen, setIsOpen] = useState(true);
+  const [urls, setUrls] = useState<Tab[]>([]);
+  const [isOpen, setIsOpen] = useState(false);
+  const [query, setQuery] = useState("");
+
+  useEffect(() => {
+    async function handleBackground(request: MessageFromBackground) {
+      console.log("asd");
+      if (request.type === MessageFromBackgroundType.TOGGLE_MENU) {
+        if (!isOpen) {
+          setIsOpen(true);
+          setUrls(request.tabs || []);
+        } else {
+          setIsOpen(false);
+        }
+      } else if (request.type === MessageFromBackgroundType.USER_SWITCHES_TAB) {
+        setIsOpen(false);
+      }
+    }
+    browser.runtime.onMessage.addListener(handleBackground);
+
+    return () => {
+      browser.runtime.onMessage.removeListener(handleBackground);
+    };
+  }, []);
+
+  useEffect(() => {
+    const fetchUrls = async () => {
+      const message: MessageFromScript = {
+        type: MessageFromScriptType.REQUEST_SEARCH_TAB,
+        search: query,
+      };
+
+      return await browser.runtime.sendMessage(message);
+    };
+
+    fetchUrls().then(setUrls);
+  }, [query]);
+
   return (
     <div>
       <Dialog
@@ -13,7 +57,6 @@ function CommandPalette() {
         className="fixed inset-0 pt-[25svh] z-50"
       >
         <Dialog.Overlay className="fixed inset-0 bg-gray-500/20" />
-
         <Combobox
           as="div"
           className="relative bg-white w-[60vw] mx-auto rounded-md shadow-2xl ring-1 ring-black/5 divide-y divide-gray-100"
@@ -24,7 +67,9 @@ function CommandPalette() {
             <Combobox.Input
               className="w-full border-0 bg-transparent outline-none text-lg text-gray-800 placeholder:text-gray-400"
               placeholder="Search ..."
-              onChange={() => {}}
+              onChange={(event) => {
+                setQuery(event.target.value);
+              }}
             />
           </div>
           <Combobox.Options
@@ -33,13 +78,21 @@ function CommandPalette() {
           >
             {urls.map((url) => {
               return (
-                <Combobox.Option value={url}>
+                <Combobox.Option value={url} key={url.key}>
                   {({ active }) => {
                     return (
                       <div
-                        className={`p-1.5 rounded ${active ? "bg-indigo-600" : "bg-white"}`}
+                        className={`flex p-1.5 gap-4 rounded ${active ? "bg-neutral-100" : "bg-white"}`}
                       >
-                        {url}
+                        <span className="flex-shrink-0 w-5 flex justify-end font-bold">
+                          {url.key}
+                        </span>
+                        <img
+                          className="w-5 h-5"
+                          src={url.favIconUrl}
+                          alt="icon"
+                        />
+                        <span>{url.title}</span>
                       </div>
                     );
                   }}
