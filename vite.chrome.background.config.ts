a9da2eb581110473f614af8f580982c17d62755e
Franck Hu <byfranckhu@outlook.com>
2024-02-01 03:13:12 +0100

    build: setup cross-env react

--- vite.chrome.background.config.ts
+++ vite.chrome.background.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  plugins: [react()],
   build: {
     emptyOutDir: false,
     outDir: resolve(__dirname, "dist"),
