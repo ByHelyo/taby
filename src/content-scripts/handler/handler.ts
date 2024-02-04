cc02e0ff4b3d383d476f873906042610d88df977
Franck Hu <byfranckhu@outlook.com>
2024-02-04 04:30:01 +0100

    style: fix command palette shadow

--- src/content-scripts/handler/handler.ts
+++ src/content-scripts/handler/handler.ts
@@ -7,8 +7,9 @@ const light = [
   ["--item-active", "#EFF0F1"],
   ["--hr-color", "#F3F4F6"],
   ["--input-color", "#191C18"],
-  ["--input-placeholder-color", "#BEBFC0"],
+  ["--input-placeholder-color", "#56566A"],
   ["--icon-filter", "none"],
+  ["--ring-color", "rgb(0, 0, 0, 0.25)"],
 ];
 
 const dark = [
@@ -16,13 +17,13 @@ const dark = [
   ["--background", "#262733"],
   ["--item-active", "#313240"],
   ["--input-color", "#E4E5F1"],
-  ["--input-placeholder-color", "#58576A"],
+  ["--input-placeholder-color", "#D1D1D1"],
   ["--hr-color", "#313141"],
-
   [
     "--icon-filter",
     "invert(100%) sepia(0%) saturate(0%) hue-rotate(93deg) brightness(103%) contrast(103%)",
   ],
+  ["--ring-color", "rgb(255, 255, 255, 0.25)"],
 ];
 
 export const handleChangeAppearance = function (
