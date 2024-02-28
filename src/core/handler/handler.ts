b1f6770d8f74668bde36d85b5c310777a7c387bd
Franck Hu <byfranckhu@outlook.com>
2024-02-28 03:38:21 +0100

    style: glass icon

--- src/core/handler/handler.ts
+++ src/core/handler/handler.ts
@@ -7,10 +7,13 @@ const light = [
   ["--hr-color", "#F3F4F6"],
   ["--input-color", "#191C18"],
   ["--input-placeholder-color", "#56566A"],
-  ["--icon-filter", "none"],
   ["--ring-color", "rgb(216, 216, 216)"],
   ["--icon-color", "#494949"],
   ["--border-color", "#b5bfd9"],
+  [
+    "--icon-filter",
+    "invert(32%) sepia(9%) saturate(951%) hue-rotate(202deg) brightness(97%) contrast(86%)",
+  ],
 ];
 
 const dark = [
@@ -20,13 +23,13 @@ const dark = [
   ["--input-color", "#E4E5F1"],
   ["--input-placeholder-color", "#D1D1D1"],
   ["--hr-color", "#313141"],
-  [
-    "--icon-filter",
-    "invert(100%) sepia(0%) saturate(0%) hue-rotate(93deg) brightness(103%) contrast(103%)",
-  ],
   ["--ring-color", "rgba(73, 75, 83, 0.898"],
   ["--icon-color", "#f3f4f6"],
   ["--border-color", "#364469"],
+  [
+    "--icon-filter",
+    "invert(94%) sepia(0%) saturate(40%) hue-rotate(189deg) brightness(94%) contrast(83%)",
+  ],
 ];
 
 export const handleChangeAppearance = function (
