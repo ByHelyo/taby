a3c3b0ff23682922449cb5159b624d76a4873635
Franck Hu <byfranckhu@outlook.com>
2024-03-28 04:01:20 +0100

    fix: settings update position

--- src/popup/settings/settings.ts
+++ src/popup/settings/settings.ts
@@ -3,11 +3,19 @@ import { Appearance, Context, PopupWindow, Storage } from "../../type/misc.ts";
 import { popup_setup } from "../../core/setup/popup.ts";
 
 const handleSelectAppearance = async function (theme: Appearance) {
-  await browser.storage.local.set({ appearance: theme });
+  await browser.storage.local.set({ [Storage.Appearance]: theme });
 };
 
 const handleSelectPopupWindow = async function (variant: PopupWindow) {
-  await browser.storage.local.set({ popup_window: variant });
+  await browser.storage.local.set({ [Storage.PopupWindow]: variant });
+};
+
+const handleSelectPositionInline = async function (value: string) {
+  await browser.storage.local.set({ [Storage.PositionInline]: value });
+};
+
+const handleSelectPositionBlock = async function (value: string) {
+  await browser.storage.local.set({ [Storage.PositionBlock]: value });
 };
 
 async function main() {
@@ -19,36 +27,64 @@ async function main() {
   const storage = await browser.storage.local.get([
     Storage.Appearance,
     Storage.PopupWindow,
+    Storage.PositionInline,
+    Storage.PositionBlock,
   ]);
-  const lightButton = document.querySelector<HTMLInputElement>(
+  const light_button = document.querySelector<HTMLInputElement>(
     "div:nth-child(1) input",
   )!;
-  const darkButton = document.querySelector<HTMLInputElement>(
+  const dark_button = document.querySelector<HTMLInputElement>(
     "div:nth-child(2) input",
   )!;
-  const popup_window =
-    document.querySelector<HTMLInputElement>(".option input")!;
+  const popup_window = document.querySelector<HTMLInputElement>(
+    ".option:nth-of-type(2) input",
+  )!;
+  const position_inline = document.querySelector<HTMLInputElement>(
+    ".option:nth-of-type(3) input",
+  )!;
+  const position_inline_label = document.querySelector<HTMLSpanElement>(
+    ".option:nth-of-type(3) span",
+  )!;
+  const position_block = document.querySelector<HTMLInputElement>(
+    ".option:nth-of-type(4) input",
+  )!;
+  const position_block_label = document.querySelector<HTMLSpanElement>(
+    ".option:nth-of-type(4) span",
+  )!;
 
   if (storage[Storage.Appearance] === Appearance.Light) {
-    lightButton.checked = true;
+    light_button.checked = true;
   } else {
-    darkButton.checked = true;
+    dark_button.checked = true;
   }
 
   if (storage[Storage.PopupWindow] == PopupWindow.Fixed) {
     popup_window.checked = true;
   }
 
-  lightButton.addEventListener("click", function () {
+  position_inline.value = storage[Storage.PositionInline];
+  position_inline_label.textContent = storage[Storage.PositionInline] + "%";
+  position_block.value = storage[Storage.PositionBlock];
+  position_block_label.textContent = storage[Storage.PositionBlock] + "%";
+
+  light_button.addEventListener("click", function () {
     handleSelectAppearance(Appearance.Light);
   });
-  darkButton.addEventListener("click", function () {
+  dark_button.addEventListener("click", function () {
     handleSelectAppearance(Appearance.Dark);
   });
   popup_window.addEventListener("click", function () {
     popup_window.checked
       ? handleSelectPopupWindow(PopupWindow.Fixed)
       : handleSelectPopupWindow(PopupWindow.UnFixed);
+  });
+  position_inline.addEventListener("input", function () {
+    handleSelectPositionInline(position_inline.value);
+    position_inline_label.textContent = position_inline.value + "%";
+  });
+  position_block.addEventListener("input", function () {
+    handleSelectPositionBlock(position_block.value);
+    position_block_label.textContent = position_block.value + "%";
   });
 
   await promise;
